d5833f82cf9fbee3e25fe8d4301cfffca40f9ae9
dr-dead <drdead7sfera@yandex.ru>
2010-10-30 00:49:01 +0400

    initial commit

---
+++ YASM.lua
@@ -0,0 +1,48 @@
+﻿--frame init
+local yasm = CreateFrame("Frame")
+yasm:SetScript('OnEvent', function(self, event, ...) self[event](self, ...) end)
+yasm:RegisterEvent("PLAYER_LOGIN")
+
+--minimap voodoo
+Minimap:ClearAllPoints()
+Minimap:SetPoint("CENTER", UIParent, "CENTER", 15, 15)
+MinimapCluster:EnableMouse(false)
+MinimapZoomIn:Hide()
+MinimapZoomOut:Hide()
+MinimapBorder:Hide()
+MinimapBorderTop:Hide()
+MinimapNorthTag:SetAlpha(0)
+MiniMapWorldMapButton:Hide()
+GameTimeFrame:Hide()
+Minimap:SetMaskTexture([=[Interface\ChatFrame\ChatFrameBackground]=])
+
+--title
+MinimapZoneTextButton:SetParent(Minimap)
+MinimapZoneTextButton:SetPoint('TOP', Minimap, 0, 16)
+MinimapZoneTextButton:SetFrameStrata("LOW")
+MinimapZoneText:SetShadowOffset(0, 0)	
+MinimapZoneText:SetAllPoints(MinimapZoneTextButton)
+
+--tracking
+MiniMapTracking:ClearAllPoints()
+MiniMapTracking:SetParent(Minimap)
+MiniMapTracking:SetPoint("TOPLEFT", 2, -2)
+MiniMapTrackingBackground:Hide()
+MiniMapTrackingButtonBorder:SetTexture(nil)
+MiniMapTrackingButton:SetHighlightTexture(nil)
+MiniMapTrackingIconOverlay:SetTexture(nil)
+MiniMapTrackingIcon:SetTexCoord(0.065, 0.935, 0.065, 0.935)
+
+--lfg
+
+--bg
+
+--clock
+TimeManager_LoadUI()
+TimeManagerClockButton:GetRegions():Hide()
+
+--mail
+
+function yasm:PLAYER_LOGIN()
+
+end

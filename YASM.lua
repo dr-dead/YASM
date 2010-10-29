9c1bf66a3f87824537fd0da48d48c2c7d8a39d32
dr-dead <drdead7sfera@yandex.ru>
2010-10-30 01:58:05 +0400

    mail icon position fix. Good old everywhere-used functions for clock frame to track pending invites and rightclick for a calendar

--- YASM.lua
+++ YASM.lua
@@ -2,6 +2,7 @@
 local yasm = CreateFrame("Frame")
 yasm:SetScript('OnEvent', function(self, event, ...) self[event](self, ...) end)
 yasm:RegisterEvent("PLAYER_LOGIN")
+yasm:RegisterEvent("CALENDAR_UPDATE_PENDING_INVITES")
 
 --minimap voodoo
 Minimap:ClearAllPoints()
@@ -16,6 +17,7 @@ MiniMapWorldMapButton:Hide()
 GameTimeFrame:Hide()
 MiniMapLFGFrameBorder:Hide()
 MiniMapBattlefieldBorder:Hide()
+MiniMapMailBorder:Hide()
 Minimap:SetMaskTexture([=[Interface\ChatFrame\ChatFrameBackground]=])
 
 --title
@@ -52,9 +54,33 @@ TimeManagerClockButton:ClearAllPoints()
 TimeManagerClockButton:SetPoint("BOTTOM", Minimap, "BOTTOM", 0, -5)
 TimeManagerClockTicker:SetPoint('CENTER', TimeManagerClockButton)
 TimeManagerClockTicker:SetFont("Fonts\\FRIZQT__.TTF", 12, "OUTLINE")
+TimeManagerClockButton:SetScript('OnClick', function(self, button)
+	if(button == 'RightButton') then
+		ToggleCalendar()
+	else
+		if(self.alarmFiring) then
+			PlaySound('igMainMenuQuit')
+			TimeManager_TurnOffAlarm()
+		else
+			ToggleTimeManager()
+		end
+	end
+end)
 
 --mail
+MiniMapMailFrame:ClearAllPoints()
+MiniMapMailFrame:SetParent(Minimap)
+MiniMapMailFrame:SetPoint("BOTTOMRIGHT", Minimap, "BOTTOMRIGHT", 4, -4)
+MiniMapMailIcon:SetTexture([=[Interface\Minimap\Tracking\Mailbox]=])
 
 function yasm:PLAYER_LOGIN()
+	yasm.CALENDAR_UPDATE_PENDING_INVITES()
+end
 
+function yasm:CALENDAR_UPDATE_PENDING_INVITES()
+	if(CalendarGetNumPendingInvites() ~= 0) then
+		TimeManagerClockTicker:SetTextColor(0, 1, 0)
+	else
+		TimeManagerClockTicker:SetTextColor(1, 1, 1)
+	end
 end
